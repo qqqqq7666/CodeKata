7e99aa0788754e15d3b305f5492a397af4573db3
rudcjs <rudcjs335@gmail.com>
2025-09-22 10:54:42 +0900

    68. 햄거버 만들기

--- 68. 햄버거 만들기.java
+++ 68. 햄버거 만들기.java
@@ -1,6 +1,25 @@
+import java.util.Stack;
+
 class Solution {
     public int solution(int[] ingredient) {
         int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i : ingredient) {
+            stack.push(i);
+            if (stack.size() >= 4) {
+                int len = stack.size();
+                if (stack.get(len - 4) == 1
+                        && stack.get(len - 3) == 2
+                        && stack.get(len - 2) == 3
+                        && stack.get(len - 1) == 1) {
+                    
+                    answer++;
+                    for(int j = 0; j < 4; j++) {
+                        stack.pop();
+                    }
+                }
+            }
+        }
         return answer;
     }
 }
