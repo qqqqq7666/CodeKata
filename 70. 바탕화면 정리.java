468a2e8a7fe2e55642586522a062981311b95bc9
rudcjs <rudcjs335@gmail.com>
2025-09-24 10:27:51 +0900

    70. 바탕화면 정리

--- 70. 바탕화면 정리.java
+++ 70. 바탕화면 정리.java
@@ -3,41 +3,65 @@ class Solution {
         int[] answer = new int[4];
         int len = wallpaper.length;
         int wid = wallpaper[0].length();
-        char[][] arr = new char[wid][];
-        for (int i = 0; i < wid; i++) 
+        char[][] arr = new char[len][wid];
+        for (int i = 0; i < len; i++)
             arr[i] = wallpaper[i].toCharArray();
-        
-        for (int i = 0; i < wid; i++) {
-            for (int j = 0; j < len; j++) {
-                if(arr[i][j] == '#'){
-                    answer[0] = i;
-                    break;
-                }
-            }
-        }
+
         for (int i = 0; i < len; i++) {
             for (int j = 0; j < wid; j++) {
-                if(arr[j][i] == '#'){
-                    answer[1] = j;
-                    break;
-                }
+                System.out.print(arr[i][j] + " ");
             }
+            System.out.println("");
         }
-        for (int i = wid - 1; i >= 0; i--) {
-            for (int j = len - 1; j >= 0; j--) {
-                if(arr[i][j] == '#'){
-                    answer[2] = i;
+
+        for (int i = 0; i < len; i++) {
+            boolean flag = false;
+            for (int j = 0; j < wid; j++) {
+                if (arr[i][j] == '#') {
+                    System.out.println(i + " " + j);
+                    answer[0] = i;
+                    flag = true;
                     break;
                 }
             }
+            if (flag)
+                break;
+        }
+        for (int i = 0; i < wid; i++) {
+            boolean flag = false;
+            for (int j = 0; j < len; j++) {
+                if (arr[j][i] == '#') {
+                    answer[1] = i;
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag)
+                break;
         }
         for (int i = len - 1; i >= 0; i--) {
+            boolean flag = false;
             for (int j = wid - 1; j >= 0; j--) {
-                if(arr[i][j] == '#'){
-                    answer[3] = j;
+                if (arr[i][j] == '#') {
+                    answer[2] = i + 1;
+                    flag = true;
                     break;
                 }
             }
+            if (flag)
+                break;
+        }
+        for (int i = wid - 1; i >= 0; i--) {
+            boolean flag = false;
+            for (int j = len - 1; j >= 0; j--) {
+                if (arr[j][i] == '#') {
+                    answer[3] = i + 1;
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag)
+                break;
         }
         return answer;
     }
